6368df5c54228768bf4363cab72fc8ed853f376f
Rrogntudju <rrogntudju@example.com>
2023-03-05 12:46:25 -0500

    wip

---
+++ crates/tauri/src/fournisseur.rs
@@ -0,0 +1,51 @@
+
+
+const ID_MS: &str = include_str!("../../../secrets/clientid.microsoft");
+const SECRET_MS: &str = include_str!("../../../secrets/secret.microsoft");
+const ID_GG: &str = include_str!("../../../secrets/clientid.google");
+const SECRET_GG: &str = include_str!("../../../secrets/secret.google");
+const AUTH_MS: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
+const AUTH_GG: &str = "https://accounts.google.com/o/oauth2/v2/auth";
+const TOKEN_MS: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/token";
+const TOKEN_GG: &str = "https://oauth2.googleapis.com/token";
+const INFOS_MS: &str = "https://graph.microsoft.com/oidc/userinfo";
+const INFOS_GG: &str = "https://openidconnect.googleapis.com/v1/userinfo";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Fournisseur {
+    Microsoft,
+    Google,
+}
+
+impl fmt::Display for Fournisseur {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fournisseur = match self {
+            Fournisseur::Microsoft => "Microsoft",
+            Fournisseur::Google => "Google",
+        };
+        f.write_str(fournisseur)
+    }
+}
+
+impl Fournisseur {
+    fn endpoints(&self) -> (&str, &str) {
+        match self {
+            Self::Microsoft => (AUTH_MS, TOKEN_MS),
+            Self::Google => (AUTH_GG, TOKEN_GG),
+        }
+    }
+
+    fn secrets(&self) -> (&str, &str) {
+        match self {
+            Self::Microsoft => (ID_MS, SECRET_MS),
+            Self::Google => (ID_GG, SECRET_GG),
+        }
+    }
+
+    fn userinfos(&self) -> &str {
+        match self {
+            Self::Microsoft => INFOS_MS,
+            Self::Google => INFOS_GG,
+        }
+    }
+}
--- crates/tauri/src/main.rs
+++ crates/tauri/src/main.rs
@@ -1,15 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+mod pkce;
+use pkce::Pkce;
+
+mod fournisseur;
+use fournisseur::Fournisseur;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
+async fn get_userinfos(fournisseur: &str) -> Result<String, String> {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_userinfos])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
